4f96821d890e6eb16b1a5d5b8d714c7cc71a72f7
Yuefan Zhang <yzhang814@myseneca.ca>
2025-05-20 09:45:31 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Yuefan Zhang
+# Author ID: 104253232
+# Date Created: 2025/05/20
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
+
