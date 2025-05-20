c80255eded31d3a7c11289b6d6d45c21cf2fbb19
Yuefan Zhang <yzhang814@myseneca.ca>
2025-05-20 09:40:45 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Yuefan Zhang
+# Author ID: 104253232
+# Date Created: 2025/05/20
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
